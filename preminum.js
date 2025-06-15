03c3575297c95eaf556f4393df4c4b80d607d4c0
vannn-15 <van1582005@gmail.com>
2025-06-15 23:52:18 +0700

    Create preminum.js

---
+++ preminum.js
@@ -0,0 +1,11 @@
+document.querySelector('.btn-back').addEventListener('click', () => {
+  alert('Back button clicked!');
+});
+
+document.querySelector('.btn-primary').addEventListener('click', () => {
+  alert('Getting started with Premium!');
+});
+
+document.querySelector('.btn-secondary').addEventListener('click', () => {
+  alert('Showing other plans...');
+});
