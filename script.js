947d9c417649894b23575dcb68075e62c7e9f4cc
tvy33 <tvy12825@gmail.com>
2025-06-17 23:43:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,13 @@
 const focusPlaylists = [
-  {
-    title: 'Peaceful Piano',
-    desc: 'Relax and indulge with beautiful piano pieces.',
-    img: 'imghome/piano.png'
-  },
-  {
-    title: 'Deep Focus',
-    desc: 'Keep calm and focus with ambient and post-',
-    img: 'imghome/foc.jpg'
-  },
-  {
-    title: 'Instrumental Study',
-    desc: 'Soft study music in the background.',
-    img: 'imghome/swm.jpg'
-  }
+  { title: 'Peaceful Piano', desc: 'Relax and indulge with beautiful piano pieces.', img: 'imghome/piano.png' },
+  { title: 'Deep Focus',     desc: 'Keep calm and focus with ambient and post-',          img: 'imghome/foc.jpg' },
+  { title: 'Instrumental Study', desc: 'Soft study music in the background.',             img: 'imghome/swm.jpg' }
 ];
 
 const vdlcPlaylists = [
-  {
-    title: 'Today\'s Top Hits',
-    desc: 'Ed Sheeran is on top of the world.',
-    img: 'imghome/ptg.jpg'
-  },
-  {
-    title: 'Rap GOAT!!!',
-    desc: 'New music from EMINEM.',
-    img: 'assets/eminem.jpg'
-  },
-  {
-    title: 'Road To Glory',
-    desc: 'The biggest songs of victory.',
-    img: 'imghome/road.jpg'
-  }
+  { title: "Today's Top Hits", desc: 'Ed Sheeran is on top of the world.',    img: 'imghome/ptg.jpg' },
+  { title: 'Rap GOAT!!!',       desc: 'New music from EMINEM.',                img: 'assets/eminem.jpg' },
+  { title: 'Road To Glory',     desc: 'The biggest songs of victory.',          img: 'imghome/road.jpg' }
 ];
 
 function renderPlaylists(containerId, playlists) {
@@ -40,8 +16,8 @@ function renderPlaylists(containerId, playlists) {
     const card = document.createElement('div');
     card.className = 'playlist-card';
     card.innerHTML = `
-      <img src=\"${p.img}\" alt=\"${p.title}\">
-      <div class=\"info\">
+      <img src="${p.img}" alt="${p.title}">
+      <div class="info">
         <h4>${p.title}</h4>
         <p>${p.desc}</p>
       </div>
@@ -52,3 +28,10 @@ function renderPlaylists(containerId, playlists) {
 
 renderPlaylists('focus-playlists', focusPlaylists);
 renderPlaylists('vdlc-playlists', vdlcPlaylists);
+
+document.getElementById("loginBtn").addEventListener("click", () => {
+  window.location.href = "login.html";
+});
+document.getElementById("signupBtn").addEventListener("click", () => {
+  window.location.href = "signup.html";
+});
